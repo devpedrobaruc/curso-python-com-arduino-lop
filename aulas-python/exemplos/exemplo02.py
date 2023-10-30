791399ab09672e60dac4dd5806c4117213343c7e
Pedro Baruc <devpedrobaruc@gmail.com>
2023-10-30 20:35:07 -0300

    remoçao de funções não utilizadas

--- aulas-python/exemplos/exemplo02.py
+++ aulas-python/exemplos/exemplo02.py
@@ -1,8 +1,7 @@
 # Exemplo de loop com LDR
-from ponte import conectar, configurar_pino, alterar_estado_pino, ler_porta_analogica
+from ponte import conectar, ler_porta_analogica
 
 conectar('COM3')
-configurar_pino(9, 'saida')
 
 luz = ler_porta_analogica()
 while luz < 500:
