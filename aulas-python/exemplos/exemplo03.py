09214238fbb99f57962fbb5dd312d841ddbf36b6
Pedro Baruc <devpedrobaruc@gmail.com>
2023-10-30 20:35:38 -0300

    exemplo modificado

--- aulas-python/exemplos/exemplo03.py
+++ aulas-python/exemplos/exemplo03.py
@@ -7,11 +7,16 @@ configurar_pino(9, 'saida')
 while True:
     luz = ler_porta_analogica()
     if (luz > 550):
+        alterar_estado_pino(9, False)
         print('LUZ DEMAIS!!!')
         break
     elif (luz > 400):
+        alterar_estado_pino(9, False)
         print('CUIDADO, LUZ!!!')
+        continue
     elif (luz > 300):
         print('MUITA LUZ')
     else:
         print('POUCA LUZ')
+
+    alterar_estado_pino(9, True)
