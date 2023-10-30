7490c9e7bf38111e7812b29ed09b01da04917799
Pedro Baruc <devpedrobaruc@gmail.com>
2023-10-30 20:21:18 -0300

    exemplo 01

---
+++ aulas-python/exemplos/exemplo01.py
@@ -0,0 +1,11 @@
+# Exemplo de if e else com LDR
+from ponte import conectar, configurar_pino, alterar_estado_pino, ler_porta_analogica
+
+conectar('COM3')
+configurar_pino(9, 'saida')
+
+luz = ler_porta_analogica()
+if (luz > 500):
+    print('MUITA LUZ')
+else:
+    print('POUCA LUZ')
